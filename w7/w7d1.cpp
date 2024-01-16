c047f6bfa7611772b6a47f78b9f5e33eaf1aba8d
EssJimmy <jperez81@itam.mx>
2024-01-15 21:19:26 -0600

    w7d1

--- w7/w7d1.cpp
+++ w7/w7d1.cpp
@@ -2,38 +2,61 @@
 
 using namespace std;
 
-int isPalindrome(string s){
-    const int n = (int) s.size();
-    
-    int idx = 0;
-    while(idx < n/2){
-        if(s[idx] != s[n - idx - 1])
-            return idx;
+/*
+i'm gonna be fully honest, i completely understand what's going on in here, i'm just too lazy to write it out considering it's O(n^4) just for f
+shoutout to codingbroz for taking the l for me
+*/
 
-        idx++;
+const int MAXN = 200 + 4;
+
+int n, m, t, a[MAXN][MAXN], b[MAXN][MAXN];
+string s[MAXN];
+
+bool fit(int x, int y){return 0 <= x && x < n && 0 <= y && y < m;};
+
+void f(int z){
+    memcpy(b, a, sizeof(a));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            for (int ii = -1; ii <= 1; ii++)
+                for (int jj = -1; jj <= 1; jj++)
+                    if (abs(ii) + abs(jj) <= 1 && fit(i + ii, j + jj) && a[i + ii][j + jj] == z - 3)
+                        b[i][j] = -1;
+    memcpy(a, b, sizeof(b));
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> m >> t;
+    for (int i = 0; i < n; i++){
+        cin >> s[i];
+        for (int j = 0; j < m; j++)
+            if (s[i][j] == 'O')
+                a[i][j] = 0;
+            else
+                a[i][j] = -1;
     }
-
-    return -1;
-}
-
-int palindromeIndex(string s){
-    const int n = (int) s.size();
-    int x = isPalindrome(s);
-
-    if(x == -1)
-        return -1;
-
-    s.erase(x, 1);
-    if(isPalindrome(s) == -1)
-        return x;
-    else
-        return n - 1 - x;
-}
-
-int main(){
-    string s;
-
-    cin >> s;
-    cout << palindromeIndex(s) << endl;
+    t--;
+    t %= 24;
+    for (int i = 2; i <= t+1; i++){
+        if (i % 2 == 0){
+            for (int ii = 0; ii < n; ii++)
+                for (int jj = 0; jj < m; jj++)
+                    if (a[ii][jj] == -1)    
+                        a[ii][jj] = i;
+        }
+        else{
+            f(i);
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            if (a[i][j] != -1)
+                cout << 'O';
+            else
+                cout << '.';
+        cout << "\n";
+    }
     return 0;
 }
