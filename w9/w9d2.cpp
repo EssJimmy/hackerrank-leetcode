29192365c6f15cbde32970217113cfdbae65a6a1
Jimmy <jperez81@itam.mx>
2024-01-31 17:25:59 -0600

    w9d2 finished

--- w9/w9d2.cpp
+++ w9/w9d2.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-long stockmax(vector<int> prices){
-    long ans = 0;
-    long stock = 0;
-    
-    for(int i = 0; i < (int) prices.size() - 1; i++){
-        stock++;
-        ans -= prices[i];
-    }
+int main(){
+    int t;
+    cin >> t;
 
-    return ans;
+    while(t--){
+        long long n, i, idx;
+        long long sum = 0;
+        vector<long long> a(50005);
+        vector<long long> b(50005);
+
+        cin >> n;
+        for(i = 1; i <= n; i++)
+            cin >> a[i];
+
+        idx = n;
+        b[n] = n;
+        for(i = n - 1; i > 0; i--){
+            if(a[idx] < a[i])
+                idx = i;
+            
+            b[i] = idx;
+        }
+
+        for(i = 1; i <= n; i++){
+            if((a[b[i]] - a[i]) >= 0)
+                sum += a[b[i]] - a[i];
+        }
+
+        cout << sum << endl;
+    }
 }
