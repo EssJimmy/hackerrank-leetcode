de45f66f0bb2b0b230ef5d6ad984f63f1e6c63a9
Jimmy <jperez81@itam.mx>
2024-02-08 23:47:33 -0600

    w9d3 finished

--- w9/w9d3.cpp
+++ w9/w9d3.cpp
@@ -1,56 +1,37 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
-int main(int argc, char** argv){
-    int q;
-    string s = "";
-    vector<string> last(2);
-    vector<vector<string>> k;
+int main() {
 
-    cin >> q;
+    string text, arg;
+    int cmd;
+    stack<string> history;
     
-    while(q--){
-        vector<string> qs(2);
-        cin >> qs[0]; cin >> qs[1];
-        
-        k.push_back(qs);
+    cin >> cmd;
+    while (cin >> cmd) {
+        switch (cmd) {
+            case 1: // Append
+                cin >> arg;
+                history.push(text);
+                text.append(arg);
+                break;
+            case 2: // Erase
+                cin >> cmd;
+                history.push(text);
+                text.erase(text.length() - cmd);
+                break;
+            case 3: // Get
+                cin >> cmd;
+                cout << text[cmd - 1] << '\n';
+                break;
+            case 4: // Undo
+                text = move(history.top());
+                history.pop();
+                break;
+        }        
     }
-
-    for(auto t : k){
-        switch(t[0][0]){
-            case '1':
-                last[0] = "1";
-                last[1] =  to_string(s.length());
-                s += t[1];
-                break;
-
-            case '2':
-                last[0] = "2";
-                last[1] = s.substr((int) t[1][0]);
-                s = s.substr(0, s.length() - (int) t[1][0]);
-                break;
-
-            case '3':
-                cout << s[(int) t[1][0]] << endl;
-                break;
-
-            case '4':
-                if(last[0][0] == '1')
-                    s = s.substr(0, (int) last[1][0]);
-
-                else if(last[0][0] == '2')
-                    s += last[1];
-
-                last[0] = "4";
-                break;
-
-            default:
-                break;
-        }
-    }
-
     return 0;
 }
